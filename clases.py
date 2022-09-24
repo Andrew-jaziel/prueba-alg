9b17e592e0765b248f8a47ea05b59fe97674b4ae
Andrew <andrew.garcia15495@est.uca.edu.ni>
2022-09-23 19:06:25 -0600

    firs commit

---
+++ clases.py
@@ -0,0 +1,86 @@
+class Inventario:
+
+    def __init__(self, cod, nom, des, pre,min,exi):
+        self.Codigo = cod
+        self.Descripcion= des
+        self.Nombres = nom
+        self.Precios = pre
+        self.Existencia = exi
+        self.Minimo = min #Becado sera un valor booleano
+    
+    def __str__(self):
+        return f"""Codigo: {self.Codigo}
+Nombres: {self.Nombres}
+Descripcion:{self.Descripcion}
+Precios: {self.Precios}
+Existencia: {self.Existencia}
+Minimo: {self.Minimo}
+
+"""
+
+class ListadoInv:
+
+    def __init__(self):
+        self.lista =[]
+    
+    def agregarElemento(self, dato):
+        try:
+            self.lista.append(dato)
+        except Exception as ex:
+            print("Ocurrio un error al agregar: ", str(ex))
+        
+    def editarElemento(self, dato, pos):
+        try:
+            self.lista[pos]=dato
+        except Exception as ex:
+             print("Ocurrio un erro al editar:", str(ex))
+    
+    def eliminarElemento(self, pos):
+        try:
+            self.lista.remove(pos)
+        except Exception as ex:
+            print("Error al eliminar:", str(ex))
+    
+    def buscarporCodigo(self, cod):
+        try:
+            pos = 0
+            for inv in self.lista:
+                
+                if inv.Codigo == cod:
+                    print("Producto encontrado...")
+                    return inv, pos
+            else:
+                print("No se encontro...")
+            pos+=1
+        except Exception as ex:
+            print("Error al buscar elemento:", str(ex))
+
+    def buscarporNombre(self, nom):
+        try:
+            pos = 0
+            for inv in self.lista:
+                
+                if inv.Nombres == nom:
+                    print("Producto encontrado...")
+                    return inv, pos
+            else:
+                print("No se encontro...")
+            pos+=1
+        except Exception as ex:
+            print("Error al buscar elemento:", str(ex))
+
+    def buscarporPrecio(self,pre):
+        try:
+            pos = 0
+            for inv in self.lista:
+                
+                if inv.Precios == pre:
+                    print("Producto encontrado...")
+                    return inv, pos
+            else:
+                print("No se encontro...")
+            pos+=1
+        except Exception as ex:
+            print("Error al buscar elemento:", str(ex))
+
+  
---
+++ principal.py
@@ -0,0 +1,115 @@
+from clases import Inventario as inv, ListadoInv as lst
+listaInv = lst()
+
+def menu():
+    print("1. Agregar Producto")
+    print("2. Editar Producto")
+    print("3. Eliminar Producto")
+    print("4. Buscar por Codigo")
+    print("5. Buscar por Nombre")
+    print("6. Buscar por Precio")
+    print("7. Mostrar Productos")
+    print("10. Salir")
+    op = int(input("Escriba su opción: "))
+    return op
+
+def agregarRegistro():
+    print("Agregar Datos del Producto")
+    codigo = input("Código: ")
+    nombres = input("Nombres: ")
+    descripcion = input("Descripcion: ")
+    precio = int(input("precio: "))
+    minimo = int(input("Digite la existencia minima: "))
+    existencia =int(input("digite la existencia del producto: "))
+   
+    inventario = inv(codigo, nombres, descripcion,precio,minimo,existencia)
+    if existencia<minimo :
+        print("La existencia es menor que la minima")
+        print(inventario)
+    else :
+        print("la existencia es mayor que la minima")
+
+    listaInv.agregarElemento(inventario)
+
+def modificarRegistro():
+    print("Editar Registro ")
+    cod = input("Código: ")
+    inv, pos = listaInv.buscarPorCodigo(cod)
+    print(f"""Nombres Actual: {inv.Nombres}
+Precio actual: {inv.Precios}
+New Descripcion: {inv.Descripcion}""")
+
+    print("Nuevos Datos")
+    nuevoNombre = input("Nombres: ")
+    nuevoPrecio = input("Precio: ")
+    nuevoDescripcion = input("Descripcion: ")
+    newInv = inv(cod, nuevoNombre, nuevoPrecio, nuevoDescripcion,)
+    listaInv.editarElemento(newInv, pos)
+
+def eliminarRegistro():
+    print("Eliminar Registro")
+    cod = input("Código: ")
+    inv, pos = listaInv.buscarporCodigo(cod)
+    print(f"""Realmente desea eliminar el registro {inv}""")
+    resp = input("SI - NO")
+    if resp.upper()=="SI":
+        listaInv.eliminarElemento(inv)
+    else:
+        print("Operación cancelada")
+
+
+def buscarporCodigo():
+    print("Buscar Registro")
+    cod = input("Codigo: ")
+    try:
+        inv, pos = listaInv.buscarporCodigo(cod)
+ 
+        if inv.Codigo != None:
+            print(inv)
+    except:
+        print("Todo bien segui adelante")
+
+def buscarporNombre():
+    print("Buscar Registro")
+    nom = input("Nombre: ")
+    try:
+        inv, pos = listaInv.buscarporNombre(nom)
+ 
+        if inv.Nombres != None:
+            print(inv)
+    except:
+        print("Todo bien segui adelante")
+       
+       
+def buscarporPrecio():
+    print("Buscar Registro")
+    pre = int(input("Precio: "))
+    try:
+        inv, pos = listaInv.buscarporPrecio(pre)
+ 
+        if inv.Precios != None:
+            print(inv)
+    except:
+        print("Todo bien segui adelante")
+
+def mostrarRegistros():
+    for inventario in listaInv.lista:
+        print(inventario)
+        print("="*30)
+   
+def main():
+    op = 0
+    while(op!=10):
+        op = menu()
+        if op == 1: agregarRegistro()
+        elif op == 2: modificarRegistro()
+        elif op == 3: eliminarRegistro()
+        elif op == 4: buscarporCodigo()
+        elif op == 5: buscarporNombre()
+        elif op == 6: buscarporPrecio()
+        elif op == 7: mostrarRegistros()
+        elif op == 10: print("hola papu")
+        else: print("Opcion no valida")
+
+main()
+
